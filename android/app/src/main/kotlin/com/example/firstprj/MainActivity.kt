111a378cdd9f39502e3ec761697bb7129de09847
Annan Prince Koomson <annan@Annans-MacBook-Pro.local>
2023-05-27 17:43:53 -0400

    Initial commit Testing of Flutter

---
+++ android/app/src/main/kotlin/com/example/firstprj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstprj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart'
+
+    class Login extends StatefulWidget{
+  const login
+
+
+
+    }
